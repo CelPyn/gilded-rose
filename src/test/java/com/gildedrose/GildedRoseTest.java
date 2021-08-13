8c2b5a4d3172fc8fadca4b7130cea313127c8df9
Cel Pynenborg <celpynenborg@gmail.com>
2021-08-13 22:54:38 +0200

    Add AssertJ and fix build

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name).isNotNull().isEqualTo("foo");
+    }
+
+}
