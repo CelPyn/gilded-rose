1fb7c3bec51fc486709225bdfff3dcfcd630a018
Cel Pynenborg <celpynenborg@gmail.com>
2021-08-14 01:09:47 +0200

    Extract operations on item quality and sellIn to methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,44 +23,34 @@ class GildedRose {
             }
 
             if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE_PASS)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                decrementQuality(item);
             } else {
                 if (isNotOverThresholdQuality(item)) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals(BACKSTAGE_PASS)) {
                         if (item.sellIn < 11) {
-                            if (isNotOverThresholdQuality(item)) {
-                                item.quality = item.quality + 1;
-                            }
+                            incrementQuality(item);
                         }
 
                         if (item.sellIn < 6) {
-                            if (isNotOverThresholdQuality(item)) {
-                                item.quality = item.quality + 1;
-                            }
+                            incrementQuality(item);
                         }
                     }
                 }
             }
 
-            item.sellIn = item.sellIn - 1;
+            decrementSellIn(item);
 
             if (item.sellIn < 0) {
                 if (!item.name.equals(BRIE)) {
                     if (!item.name.equals(BACKSTAGE_PASS)) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
+                        decrementQuality(item);
                     } else {
-                        item.quality = 0;
+                        expireQuality(item);
                     }
                 } else {
-                    if (isNotOverThresholdQuality(item)) {
-                        item.quality = item.quality + 1;
-                    }
+                    incrementQuality(item);
                 }
             }
         }
@@ -68,5 +58,29 @@ class GildedRose {
 
     private boolean isNotOverThresholdQuality(final Item item) {
         return item.quality < QUALITY_THRESHOLD;
+    }
+
+    private boolean isNotZeroQuality(final Item item) {
+        return item.quality > 0;
+    }
+
+    private void incrementQuality(final Item item) {
+        if (isNotOverThresholdQuality(item)) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decrementQuality(final Item item) {
+        if (isNotZeroQuality(item)) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void decrementSellIn(final Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void expireQuality(final Item item) {
+        item.quality = 0;
     }
 }
