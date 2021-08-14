0ccb7dfe4db0f6fefb86e0239b11984d166b6b5d
Cel Pynenborg <celpynenborg@gmail.com>
2021-08-14 11:22:35 +0200

    Refactor magic number

--- src/main/java/com/gildedrose/QualityHelper.java
+++ src/main/java/com/gildedrose/QualityHelper.java
@@ -2,16 +2,17 @@ package com.gildedrose;
 
 public final class QualityHelper {
 
-    private static final int QUALITY_THRESHOLD = 50;
+    private static final int UPPER_QUALITY_THRESHOLD = 50;
+    private static final int LOWER_QUALITY_THRESHOLD = 0;
 
     private QualityHelper() {}
 
     public static boolean isNotOverThresholdQuality(final Item item) {
-        return item.quality < QUALITY_THRESHOLD;
+        return item.quality < UPPER_QUALITY_THRESHOLD;
     }
 
     public static boolean isNotZeroQuality(final Item item) {
-        return item.quality > 0;
+        return item.quality > LOWER_QUALITY_THRESHOLD;
     }
 
     public static void incrementQuality(final Item item, final int amount) {
@@ -20,7 +21,7 @@ public final class QualityHelper {
         }
 
         if (isNotOverThresholdQuality(item)) {
-            item.quality = item.quality + 1;
+            item.quality += 1;
             incrementQuality(item, amount - 1);
         }
     }
@@ -31,20 +32,20 @@ public final class QualityHelper {
         }
 
         if (isNotZeroQuality(item)) {
-            item.quality = item.quality - 1;
+            item.quality -= 1;
             decrementQuality(item, amount - 1);
         }
     }
 
     public static void decrementSellIn(final Item item) {
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
     }
 
     public static void expireQuality(final Item item) {
-        item.quality = 0;
+        item.quality = LOWER_QUALITY_THRESHOLD;
     }
 
     public static boolean isExpired(final Item item) {
-        return item.sellIn < 0;
+        return item.sellIn < LOWER_QUALITY_THRESHOLD;
     }
 }
